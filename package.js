1d14bba124c13619c59f20c8509e4f156148e243
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2013-08-14 11:50:26 -0300

    removing css

--- package.js
+++ package.js
@@ -6,7 +6,6 @@ Package.on_use(function (api, where) {
 
     api.use(['jquery', "underscore"], ['client', 'server']);
     api.add_files('js/mesosphere.js', ['client', 'server']);
-    api.add_files('css/mesosphere.css', 'client');
 
 });
 
